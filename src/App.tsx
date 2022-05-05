1312f61ff36df6bef0e8c763a03beac8fcc8f271
mechapear <mechapear@gmail.com>
2022-05-05 22:32:48 +0700

    fix: change theme color and improve hint message

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,21 @@ import { productList } from '@/mock-up-data'
 const App = (): JSX.Element => {
   return (
     <>
-      <div className="min-h-screen grid place-items-center">
-        <p>
-          Press <kbd>command</kbd> to open the command palette
+      <div className="grid min-h-screen place-items-center">
+        <p className="text-stone-900">
+          Press <Kbd>⌘ + K</Kbd> or <Kbd>Ctrl + K</Kbd> to open the command palette
         </p>
       </div>
       <CommandPalette productList={productList} />
     </>
+  )
+}
+
+function Kbd(props: { children: string }) {
+  return (
+    <kbd className="shadow-stone-800/15 rounded-lg border-0 bg-stone-200/75 py-1 px-2 text-sm font-bold text-stone-700 shadow">
+      {props.children}
+    </kbd>
   )
 }
 
